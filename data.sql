33b372866c60eaded1e6bba5a69b2bd068415b27
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-08-15 15:27:14 +0300

    Create database for vet_clinic.

---
+++ data.sql
@@ -0,0 +1,5 @@
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Augmon', '2020-2-3', 0 , TRUE, 10.23),
+	   ('Gabumon', '2018-11-15', 2 , TRUE, 8),
+	   ('Pikachu', '2021-1-7', 1, FALSE, 15.04),
+	   ('Devimon', '2017-5-12', 5, TRUE, 11);
