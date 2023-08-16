118634ff1ac35f3532300d01ecbc7e4ad8d5a288
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-08-16 20:22:52 +0300

    Add reference key and join tables.

--- data.sql
+++ data.sql
@@ -15,3 +15,38 @@ VALUES ('Charmander', '2020-02-08', 0 , FALSE, 11,'Animal'),
 	   ('Boarmon', '2005-01-07', 7, TRUE, 20.4, 'Animal'),
 	   ('Blossom', '1998-10-13', 3, TRUE, 17, 'Animal'),
 	   ('Ditto', '2022-05-14', 4, TRUE, 22, 'Animal');
+
+-- Day 3
+
+-- Insert the following data into the owners table:
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+	   ('Jennifer Orwell', 19),
+	   ('Bob', 45),
+	   ('Melody Pond', 77),
+	   ('Dean Winchester', 14),
+	   ('Jodie Whittaker', 38);
+
+-- Insert the following data into the species table:
+
+INSERT INTO species (name)
+VALUES ('Pokemon'),
+	   ('Digimon');
+
+-- Modify your inserted animals so it includes the species_id value:
+
+UPDATE animals
+SET species_id = 
+    CASE 
+        WHEN name LIKE '%mon' THEN 2
+        ELSE 1
+    END;
+
+-- Modify your inserted animals to include owner information (owner_id):
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith') WHERE name = 'Agumon';
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell') WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob') WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name IN ('Angemon', 'Boarmon');
--- schema.sql
+++ schema.sql
@@ -13,3 +13,33 @@ weight_kg DECIMAL
 
 ALTER TABLE animals
 ADD species VARCHAR(255);
+
+-- Day three
+
+-- Create a table named owners with the following columns:
+CREATE TABLE owners (
+id SERIAL PRIMARY KEY,
+full_name VARCHAR(255) NOT NULL,
+age INT
+);
+
+-- Create a table named species with the following columns:
+
+CREATE TABLE species(
+id SERIAL PRIMARY KEY,
+name VARCHAR(255) NOT NULL
+);
+
+-- Day 3
+
+-- Remove column species
+
+ALTER TABLE animals DROP column species;
+
+-- Add column species_id which is a foreign key referencing species table
+
+ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
+
+-- Add column owner_id which is a foreign key referencing the owners table
+
+ALTER TABLE animals ADD COLUMN owner_id INTEGER REFERENCES owners(id);
