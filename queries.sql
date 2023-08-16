c1f2cefb860bc057b62a2a921377444fd5dee6fe
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-08-16 13:42:56 +0300

    Add data and logic to my server.

--- queries.sql
+++ queries.sql
@@ -36,3 +36,95 @@ WHERE name != 'Gabumon';
 
 SELECT * FROM animals
 WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+-- Day 2
+
+-- Update the animals table and then roll back
+
+BEGIN;
+UPDATE animals SET species = NULL;
+ROLLBACK;
+
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species = NULL;
+
+COMMIT;
+
+--  Inside a transaction delete all records in the animals table, then roll back the transaction.
+
+BEGIN;
+DELETE FROM animals
+ROLLBACK;
+
+-- Delete all animals born after Jan 1st, 2022.
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01'
+
+-- Create a savepoint for the transaction.
+
+SAVEPOINT sv1;
+
+--Update all animals' weight to be their weight multiplied by -1.
+
+UPDATE animals SET weight_kg = weight_kg * '-1';
+
+-- Rollback to the savepoint
+
+ROLLBACK TO SV1;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+
+UPDATE animals SET weight_kg = weight_kg * '-1'
+WHERE weight_kg < 0;
+
+-- Commit transaction
+
+COMMIT;
+
+-- How many animals are there?
+
+SELECT COUNT(name) FROM animals;
+
+-- How many animals have never tried to escape?
+
+SELECT COUNT(escape_attempts) FROM animals
+WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+
+SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+
+SELECT
+    AVG(escape_attempts)
+FROM
+    animals
+GROUP BY neutered
+
+-- What is the minimum and maximum weight of each type of animal?
+
+SELECT
+    MAX(weight_kg), MIN(weight_kg)
+FROM
+    animals
+GROUP BY  species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+
+SELECT
+    date_of_birth, AVG(escape_attempts)
+FROM
+    animals
+GROUP BY species, date_of_birth
+HAVING date_of_birth BETWEEN '1990-01-01' AND '2000-01-01'
